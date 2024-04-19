ed542b615eb1d0b583d06e7eaa14678a002209c7
Son Le <son_le@datahouse.com>
2024-04-19 10:36:50 +0700

    feat: create dtos

--- server/src/polls/polls.controller.ts
+++ server/src/polls/polls.controller.ts
@@ -1,15 +1,18 @@
-import { Controller, Logger, Post } from '@nestjs/common';
+import { Body, Controller, Logger, Post } from '@nestjs/common';
+import { CreatePollDto, JoinPollDto } from './dtos';
 
 @Controller('polls')
 export class PollsController {
   @Post()
-  async create() {
+  async create(@Body() createPollDto: CreatePollDto) {
     Logger.log('In create!');
+    return createPollDto;
   }
 
   @Post('/join')
-  async join() {
+  async join(@Body() joinPollDto: JoinPollDto) {
     Logger.log('In join!');
+    return joinPollDto;
   }
 
   @Post('/rejoin')
